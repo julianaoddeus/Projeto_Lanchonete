84752bee43789dfd108f60e681563d56d5c7f69b
Juliana <julianaacademi@gmail.com>
2024-01-16 21:46:35 -0300

    update_00011
    migration carrinho compra lanche

--- Lanchonete.API/Lanchonete.API/obj/Debug/net8.0/Lanchonete.API.AssemblyInfo.cs
+++ Lanchonete.API/Lanchonete.API/obj/Debug/net8.0/Lanchonete.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lanchonete.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf26e0126c5091bfe47c62171dd6262bc463c522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lanchonete.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lanchonete.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
